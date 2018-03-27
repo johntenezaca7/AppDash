b4c29e71158549686d0a8fcecf67f5a34093d633
Owner <owner@MacBook-Pro.local>
2018-03-27 19:58:34 -0400

    added more style and updated landing

--- react/Landing.jsx
+++ react/Landing.jsx
@@ -3,9 +3,17 @@ import React, { Component } from "react";
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <div className="Map-Container" />
-        <h1>This is the Map</h1>
+      <div className="Landing-Photo">
+        <div className="Nav-Bar">
+          <img
+            className="Logo"
+            src="https://tmpfilecdn.freelogodesign.org/18398c4f-d687-455b-abe7-723728f3f263.png"
+            alt="logo"
+          />
+          <div className="About">
+            Box
+          </div>
+        </div>
       </div>
     );
   }
